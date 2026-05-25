f9d4ca4239caf706dd890159409a9849d16fcd2f
ELder <marruky747@gmail.com>
2026-05-25 20:48:27 +0100

    feat: /analyse endpoint with FASTA parsing and analysis

--- build/CMakeFiles/genomescan-engine.dir/src/fasta/FastaParser.cpp.o.d
+++ build/CMakeFiles/genomescan-engine.dir/src/fasta/FastaParser.cpp.o.d
@@ -810,4 +810,5 @@ CMakeFiles/genomescan-engine.dir/src/fasta/FastaParser.cpp.o: \
   /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/c++/v1/__filesystem/directory_options.h \
   /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/c++/v1/__filesystem/path_iterator.h \
   /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/c++/v1/__filesystem/recursive_directory_iterator.h \
-  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/c++/v1/__filesystem/u8path.h
+  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/c++/v1/__filesystem/u8path.h \
+  /Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include/c++/v1/sstream
